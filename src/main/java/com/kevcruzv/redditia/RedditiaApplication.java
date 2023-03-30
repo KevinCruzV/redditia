cec6773929b8e04accd6331caf0c95a23c8ca9c5
Kevin Cruz <kevin.cruzvezo1@gmail.com>
2023-03-30 15:42:49 +0200

    backend init

---
+++ src/main/java/com/kevcruzv/redditia/RedditiaApplication.java
@@ -0,0 +1,13 @@
+package com.kevcruzv.redditia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevcruzv/redditia/RedditiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevcruzv.redditia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
