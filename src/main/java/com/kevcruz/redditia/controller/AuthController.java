186e35020baf0dd0eb8b3a0a09ba25f6e3b54ed8
Kevin Cruz <kevin.cruzvezo1@gmail.com>
2023-04-07 17:00:18 +0200

    authcontroller

--- src/main/java/com/kevcruz/redditia/controller/AuthController.java
+++ src/main/java/com/kevcruz/redditia/controller/AuthController.java
@@ -1,6 +1,10 @@
 package com.kevcruz.redditia.controller;
 
 import com.kevcruz.redditia.dto.RegisterRequest;
+import com.kevcruz.redditia.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
     @PostMapping("/login")
     public void Login(){
 
     }
     @PostMapping("/register")
-    public void Register(@RequestBody RegisterRequest registerRequest){
-
+    public ResponseEntity<String> Register(@RequestBody RegisterRequest registerRequest){
+        authService.Register(registerRequest);
+        return new ResponseEntity<>("User Registration Successful", HttpStatus.OK);
     }
 }
